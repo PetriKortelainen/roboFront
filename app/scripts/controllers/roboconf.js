f36a5ca68c4212381d957bc951ee3d55abf6dbaa
Jsemi <jsemi@users.noreply.github.com>
2015-10-28 15:50:21 +0200

    new dialog start

--- app/scripts/controllers/roboconf.js
+++ app/scripts/controllers/roboconf.js
@@ -1,3 +1,4 @@
+(function () {
 'use strict';
 
 angular
@@ -17,13 +18,85 @@ angular
 			});
 	})
 
-.controller('postCtrl', function($scope, $http) {
-	$scope.postTree = function() {
-		var data = $scope.tree;
-		console.log(data);
-		$http.post('http://localhost:8080/manage-api/post/', data)
-			.success(function(data, status) {
-				//TODO
-			});
-	};
-});
+    .controller('postCtrl', ['$scope', function ($scope) {
+      $scope.remove = function (scope) {
+        scope.remove();
+      };
+
+      $scope.toggle = function (scope) {
+        scope.toggle();
+      };
+
+      $scope.moveLastToTheBeginning = function () {
+        var a = $scope.data.pop();
+        $scope.data.splice(0, 0, a);
+      };
+
+      $scope.newSubItem = function (scope) {
+        var nodeData = scope.$modelValue;
+        nodeData.nodes.push({
+          id: nodeData.id * 10 + nodeData.nodes.length,
+          title: nodeData.title + '.' + (nodeData.nodes.length + 1),
+          nodes: []
+        });
+      };
+
+      $scope.collapseAll = function () {
+        $scope.$broadcast('collapseAll');
+      };
+
+      $scope.expandAll = function () {
+        $scope.$broadcast('expandAll');
+      };
+
+      $scope.data = [{
+        'id': 1,
+        'title': 'node1',
+        'nodes': [
+          {
+            'id': 11,
+            'title': 'node1.1',
+            'nodes': [
+              {
+                'id': 111,
+                'title': 'node1.1.1',
+                'nodes': []
+              }
+            ]
+          },
+          {
+            'id': 12,
+            'title': 'node1.2',
+            'nodes': []
+          }
+        ]
+      }, {
+        'id': 2,
+        'title': 'node2',
+        'nodrop': true, // An arbitrary property to check in custom template for nodrop-enabled
+        'nodes': [
+          {
+            'id': 21,
+            'title': 'node2.1',
+            'nodes': []
+          },
+          {
+            'id': 22,
+            'title': 'node2.2',
+            'nodes': []
+          }
+        ]
+      }, {
+        'id': 3,
+        'title': 'node3',
+        'nodes': [
+          {
+            'id': 31,
+            'title': 'node3.1',
+            'nodes': []
+          }
+        ]
+      }];
+    }]);
+
+}());
