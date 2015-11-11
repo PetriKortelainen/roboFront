1a933019f57c0f4c95beb082a802276357b45228
Axel Auvinen <axel.auvinen@gmail.com>
2015-11-11 14:27:34 +0200

    Fixed ngDialog initalization

--- app/scripts/directives/robo-question.js
+++ app/scripts/directives/robo-question.js
@@ -5,9 +5,9 @@
 		.module('robottiFrontApp')
 		.directive('roboQuestion', roboQuestion);
 
-	roboQuestion.$inject = ['$rootScope', 'questionService'];
+	roboQuestion.$inject = ['$rootScope', 'questionService', 'ngDialog'];
 
-	function roboQuestion($rootScope, questionService) {
+	function roboQuestion($rootScope, questionService, ngDialog) {
 		var directive = {
 			restrict: 'E',
 			scope: {},
@@ -18,7 +18,38 @@
 				$scope.answerQuestion = function(answer) {
 					$rootScope.currentQuestion = answer.id_next_question;
 					$scope.question = questionService.getQuestionById($rootScope.currentQuestion);
+					if($scope.question.answer_type == 'redirect') {
+						console.log("REDIRECT HERE")
+					}
+
 				}
+
+				$scope.openQuestionForm = function() {
+					ngDialog.openConfirm({template: 'dialogtest.html',
+
+					}).then(
+						function(value) {
+							//You need to implement the saveForm() method which should return a promise object
+							$scope.saveForm().then(
+								function(success) {
+									ngDialog.open({template: '<div class="ngdialog-message"> \
+									  Tallennus onnistui.</div>',
+										plain: 'true'
+									});
+								},
+								function(error) {
+									ngDialog.open({template: '<div class="ngdialog-message"> \
+									  Tallennuksessa on tapahtunut virhe.</div>',
+										plain: 'true'
+									});
+								}
+							);
+						},
+						function(value) {
+							//Cancel or do nothing
+						}
+					);
+				};
 			}
 
 		};
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,38 +14,10 @@
 		.module('robottiFrontApp')
 		.controller('RobotController', RobotController);
 
-	RobotController.$inject = ['$scope', 'treeService', '$log', '$rootScope', 'questionService', 'ngDialog'];
+	RobotController.$inject = ['$scope', 'treeService', '$log', '$rootScope', 'questionService'];
 
 	/* @ngInject */
-	function RobotController($scope, treeService, $log, $rootScope, questionService, ngDialog) {
-
-			$scope.openQuestionForm = function() {
-				ngDialog.openConfirm({template: 'dialogtest.html',
-
-				}).then(
-					function(value) {
-						//You need to implement the saveForm() method which should return a promise object
-						$scope.saveForm().then(
-							function(success) {
-								ngDialog.open({template: '<div class="ngdialog-message"> \
-								  Tallennus onnistui.</div>',
-									plain: 'true'
-								});
-							},
-							function(error) {
-								ngDialog.open({template: '<div class="ngdialog-message"> \
-								  Tallennuksessa on tapahtunut virhe.</div>',
-									plain: 'true'
-								});
-							}
-						);
-					},
-					function(value) {
-						//Cancel or do nothing
-					}
-				);
-			};
-
+	function RobotController($scope, treeService, $log, $rootScope, questionService) {
 
 		$scope.toggle = true;
 
