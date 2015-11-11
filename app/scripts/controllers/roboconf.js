4a817f953f68860dac118a39cb62f02c85981a50
Jsemi <jsemi@users.noreply.github.com>
2015-11-11 14:48:34 +0200

    aleksille kamelikeissi

--- app/scripts/controllers/roboconf.js
+++ app/scripts/controllers/roboconf.js
@@ -2,56 +2,56 @@
 'use strict';
 
 angular
-	.module('robottiFrontApp', ['ngRoute', 'ui.tree'])
-	.config(function($routeProvider) {
-		$routeProvider
-			.when('/', {
-				templateUrl: 'views/conf.html',
-				controller: 'postCtrl'
-			})
+  .module('robottiFrontApp', ['ngRoute', 'ui.tree'])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/conf.html',
+        controller: 'postCtrl'
+      })
       .when('/newtree', {
         templateUrl: 'views/posttree.html',
         controller: 'postCtrl'
       })
-			.otherwise({
-				redirectTo: '/'
-			});
-	})
-
-    .controller('postCtrl', ['$scope', '$http', function ($scope, $http) {
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .controller('postCtrl', ['$scope', '$http', function($scope, $http) {
 
       $scope.sendData = function() {
-        var dataObj = JSON.stringify($scope.data);
+        var temp = angular.toJson($scope.data);
+        console.log('temp: ' + temp);
+        var dataObj = temp.substring(1, temp.length-1);
         $http.post('localhost:8081/xxx', dataObj);
         console.log('sent: ' + dataObj);
       };
 
-      $scope.remove = function (scope) {
-        scope.remove();
+      $scope.remove = function($scope) {
+        $scope.remove();
       };
 
-      $scope.toggle = function (scope) {
-        scope.toggle();
+      $scope.toggle = function($scope) {
+        $scope.toggle();
       };
 
-      $scope.newSubItem = function ($scope) {
+      $scope.newSubItem = function($scope) {
         if ($scope.collapsed) {
           $scope.toggle();
         }
-
         var nodeData = $scope.$modelValue;
         nodeData.nodes.push({
           id: nodeData.id + '.' + (nodeData.nodes.length + 1),
-          answertext: '',
-          questiontext: '',
+          answerText: '',
+          questionText: '',
           nodes: []
         });
       };
 
       $scope.data = [{
         'id': '1',
-        'dialogname' : '',
-        'questiontext' : '',
+        'dialogName' : '',
+        'questionText' : '',
         'nodes': []
       }];
     }]);
